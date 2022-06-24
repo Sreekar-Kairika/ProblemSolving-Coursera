5a015b28ab49f8a3036fb4a8df48d283913a4089
Sreekar-Kairika <sreekar.kairika@zemosolabs.com>
2022-06-24 10:44:33 +0530

    naming convention

--- src/week2_CountInversions/CountInversion.java
+++ src/week2_CountInversions/CountInversion.java
@@ -58,4 +58,11 @@ class CountInversion {
 
         return count;
     }
+
+    public void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
 }
