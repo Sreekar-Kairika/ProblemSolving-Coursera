091a37d71b3ca839e048174429569300e3c0e9d8
Sreekar-Kairika <sreekar.kairika@zemosolabs.com>
2022-06-28 14:58:58 +0530

    randomised selection

--- src/week4_RandomizedAlgo/Main.java
+++ src/week4_RandomizedAlgo/Main.java
@@ -4,9 +4,11 @@ public class Main {
     public static void main(String[] args){
 
         int[] arr = {1,3,5,6,7,9};
-        int order = 2;
+        int order = 4;
+
         RandomizedSelection ob = new RandomizedSelection();
 
+        System.out.print("Element which is at " + order + " index is ");
         System.out.println(ob.randomSelection(arr,0,arr.length-1,order));
 
     }
